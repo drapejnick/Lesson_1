2ae81d4b27670c02862c1ee354061959e5d8b780
Vladimir <drapejnick@gmail.com>
2022-08-30 20:40:43 +0300

    add Example 001

---
+++ Example001/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.Write("Введите число: ");
+int number = int.Parse(Console.ReadLine());
+int sqr = number * number;
+Console.WriteLine($"Квадрат числа {number} равен = {sqr}");
+
+int sqr1 = Convert.ToInt32(Math.Pow(number, 2));
+Console.WriteLine($"Квадрат числа {number} равен = {sqr1}");
