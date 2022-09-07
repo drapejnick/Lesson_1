c0e29c965e4c8cb18a33a362cf9d2f5db9ff2855
Vladimir <drapejnick@gmail.com>
2022-09-07 17:28:16 +0300

    add example 014

---
+++ Example014/Program.cs
@@ -0,0 +1,14 @@
+﻿// Напишите программу, которая принимает на вход число (N) и выдает таблицу квадратов чисел от 1 до N
+// 5, -> 1,4,9,16,25
+// 2, -> 1,4
+
+Console.Write("Введите число ");
+int x = int.Parse(Console.ReadLine());
+int count = 1;
+
+Console.Write($"{x} -> ");
+while (count <= x)
+{
+    Console.Write($" {Math.Pow((count), 2)} ");
+    count++;
+}
