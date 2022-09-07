e59c85ed5aae72dfb502ffce8c6c5e479199c4af
Vladimir <drapejnick@gmail.com>
2022-09-07 16:31:24 +0300

    add example 012

---
+++ Example012/Program.cs
@@ -0,0 +1,23 @@
+﻿// Напишите программу, которая по заданному номеру четверти, показывает диапазон координат точек в этой четверти (x,y)
+
+Console.Write("Введите номер четверти ");
+int x = int.Parse(Console.ReadLine());
+
+if (x == 1)
+{
+    Console.WriteLine("x > 0 и y > 0");
+}
+if (x == 2)
+{
+    Console.WriteLine("x < 0 и y > 0");
+}
+if (x == 3)
+{
+    Console.WriteLine("x < 0 и y < 0");
+}
+if (x == 4)
+{
+    Console.WriteLine("x > 0 и y < 0");
+}
+
+
