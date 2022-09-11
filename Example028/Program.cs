3396e25d48911a9f1261e0e7b78c9d0f8d29d2e8
Vladimir <drapejnick@gmail.com>
2022-09-11 22:47:27 +0300

    add exercise 028

---
+++ Example028/Program.cs
@@ -0,0 +1,20 @@
+﻿// Напишите программу, которая принимает на вход число N и выдает произведение чисел от 1 до N. 
+// 4 -> 24
+// 5 -> 120
+
+Console.Clear();
+Console.Write("Введите число: ");
+int number = int.Parse(Console.ReadLine());
+
+int Factorial(int num)
+{
+    int count = num;
+    while (count > 1)
+    {
+        count--;
+        num *= count;
+    }
+    return num;
+}
+
+Console.WriteLine($"{number} -> {Factorial(number)}");
