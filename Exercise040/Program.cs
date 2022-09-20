24fa07447e78f255c045a7e1d331be5db811a462
Vladimir <drapejnick@gmail.com>
2022-09-20 17:53:08 +0300

    add example 040

---
+++ Exercise040/Program.cs
@@ -0,0 +1,26 @@
+﻿// Напишите программу, которая принимает на вход три числа и проверяет, может ли существовать треугольник 
+// со сторонами такой длины. 
+
+Console.Clear();
+Console.Write($"(Введите стороны треугольника: а, в, с )");
+string strNumber = Console.ReadLine();
+string[] points = strNumber.Split(',');
+int[] intPoints = new int[3];
+int count = 0;
+
+foreach (string el in points)
+{
+    intPoints[count] = int.Parse(el);
+    count++;
+}
+if (intPoints[0] < intPoints[1] + intPoints[2] && intPoints[1] < intPoints[0] + intPoints[2] && intPoints[2] < intPoints[1] + intPoints[0])
+{
+    Console.WriteLine("Треугольник может существовать");
+}
+else
+{
+    Console.WriteLine("Треугольник не может существовать");
+}
+
+
+
